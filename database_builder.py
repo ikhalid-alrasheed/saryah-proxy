72e21247110b44c4c7b0cc394bba8b3efab312c6
ikhalid-alrasheed <71514689+ikhalid-alrasheed@users.noreply.github.com>
2021-12-07 13:10:12 +0300

    Delete database_builder.py

--- database_builder.py
+++
@@ -1,9 +0,0 @@
-from app import *
-from pprint import pprint
-import json
-
-def reading_from_json(file):
-    with open(f'dummy/{file}', 'r') as openfile:
-        json_object = json.load(openfile)
-    return json_object
-
